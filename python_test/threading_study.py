4ba4174da6bfd7dc10c378613cecdf814d316aec
jack <jack@jack-VirtualBox>
2016-11-20 22:03:17 +0800

    review threading study

---
+++ python_test/threading_study.py
@@ -0,0 +1,27 @@
+'''
+Created on Nov 20, 2016
+
+@author: jack
+'''
+
+import threading
+import time
+
+
+def thread_job():
+    print "thread_job start"
+    time.sleep(8)
+    print "thread_job done"
+
+def main():
+#     print threading.active_count()
+#     print threading.enumerate()
+#     print threading.current_thread()
+    addT = threading.Thread(target=thread_job, name="T1")
+    addT.start()
+    addT.join()
+    print "T1 DONE"
+    
+if __name__ == '__main__':
+    main()
+    pass
