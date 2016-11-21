8ee479a9f5bd91f618ad1d32248f06fab59eb363
Jack <wangjievibeke@foxmail.com>
2016-11-21 10:43:56 +0800

    add singleton decorator

---
+++ singleton/test_singleton.py
@@ -0,0 +1,27 @@
+
+
+import unittest  
+from singleton import Singleton  
+ 
+@Singleton  
+class A(object): pass  
+  
+class TestSingleton(unittest.TestCase):  
+    def testSingleton(self):  
+        a = A.Instance()  
+        aa = A.Instance()  
+        self.assertTrue(a is aa)  
+          
+    def testSingletonCall(self):  
+        a = A() 
+        aa = A()
+        self.assertTrue(a is aa)
+        
+    def testIsinstance(self):  
+        a = A.Instance()  
+        self.assertTrue(isinstance(a, A))  
+        self.assertFalse(isinstance(a, list))  
+  
+  
+if __name__ == "__main__":  
+    unittest.main() 
