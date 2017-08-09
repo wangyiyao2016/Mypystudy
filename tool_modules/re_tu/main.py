c2c1d91753eba77b6d650d3cc8160874895462bd
Jack <wangjievibeke@foxmail.com>
2017-08-09 18:38:00 +0800

    update

--- tool_modules/re_tu/main.py
+++ tool_modules/re_tu/main.py
@@ -12,16 +12,20 @@ string = "hdfs://host:8020/user/hive/warehouse/test.db/test"
 
 pattern = r"host"
 
+pattern_test = r"hdfs://"
 #         """
 
 prog = re.compile(pattern)
+prog_test = re.compile(pattern_test)
 result_s = prog.search(string)
-result_m = prog.match(string)
+result_m = prog_test.match(string)
+result_sub = prog.sub("tsoh", string)
 
 
 def main():
     print(result_s)
     print(result_m)
+    print(result_sub)
 
 
 if __name__ == '__main__':
