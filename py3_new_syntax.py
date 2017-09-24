ef5a8e4e0a51a879fd33f1400e74d8961039892b
Jack <wangjievibeke@foxmail.com>
2017-09-25 01:30:35 +0800

    update

---
+++ py3_new_syntax.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+Created on Sep 24, 2017
+
+@author: Jack
+
+@contact:    wangjievibeke@foxmail.com
+'''
+
+
+def f_str(x: str, y: int) -> None:
+    print(f"x is {x}, y is {y}")
+    pass
+
+
+if __name__ == '__main__':
+    f_str({"d": 1}, 56545)
+    help(f_str)
+    pass
